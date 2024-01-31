c9a8d0564c4a25e58deb4e0c80d16a9f43a3b9d4
DESKTOP-UO43OJN\MatMat <matinmatmat@gmail.com>
2024-01-31 09:48:48 -0800

    Adjusting Scripts

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -10,14 +10,24 @@ public class Spawn : MonoBehaviour
 
     [SerializeField]
     float spawnrate = 2f;
+
+    [SerializeField]
+    float spawn_multiplier = 1f;
+
+
     void Start()
     {
         StartCoroutine(spawn_timer());
     }
+    void Update()
+    {
+        spawnrate -= Time.deltaTime * spawn_multiplier;
+    }
 
     IEnumerator spawn_timer()
     {
-        yield return new WaitForSeconds(spawnrate);
+
+        yield return new WaitForSeconds(Mathf.Max(0.5f, spawnrate));
         Destroy(Instantiate(ms, new Vector2(Random.Range(-4.5f , 4.5f),-7), Quaternion.identity) , 2f);
         StartCoroutine(spawn_timer());
 
