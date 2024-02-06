3438bdb345fe0f29a971f0056162543eec9455f2
peymanats <peymanat2s@gmail.com>
2024-02-07 02:08:36 +0330

    Score script done

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
+    //float pointIncreasePerSecond;
+    public float score;
+    public TMPro.TextMeshProUGUI ScoreText;
+    public float pointsPerSecond ;
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = 0f;
+        pointsPerSecond = 1f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ScoreText.text = "Score: " + (int)score;
+        score += pointsPerSecond * Time.deltaTime;
     }
 }
