e8da3fa1bdfb6436da5a4a481c61f1d99b2a33b8
DESKTOP-UO43OJN\MatMat <matinmatmat@gmail.com>
2024-02-08 12:33:09 -0800

    changing spawn rate

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -7,18 +7,29 @@ public class Spawn : MonoBehaviour
     [SerializeField]
     GameObject ms;
 
-
     [SerializeField]
     float spawnrate = 2f;
+
+    [SerializeField]
+    float spawn_subs = 0.1f;
+
+    [SerializeField]
+    float spawnrate_limit = 0.3f;
+
+
     void Start()
     {
         StartCoroutine(spawn_timer());
     }
+    void Update()
+    {
+        spawnrate -= Time.deltaTime * spawn_subs;
+    }
 
     IEnumerator spawn_timer()
     {
-        yield return new WaitForSeconds(spawnrate);
-        Destroy(Instantiate(ms, new Vector2(Random.Range(-7.3f , 7.3f),-10), Quaternion.identity) , 2f);
+        yield return new WaitForSeconds(Mathf.Max(spawnrate_limit, spawnrate));
+        Destroy(Instantiate(ms, new Vector2(Random.Range(-7.3f , 7.3f),-9.5f), Quaternion.identity) , 2f);
         StartCoroutine(spawn_timer());
 
     }
